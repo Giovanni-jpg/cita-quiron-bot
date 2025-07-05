af297680aba2892c1b2a4b310f9a16d56988e065
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2025-07-05 10:21:32 +0200

    Stop tracking local config and data files

--- config/constants.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  doctor: 'Dr. Name',
-  insurance: 'Your Insurance',
-  insurancePlan: 'Plan Name',
-  gender: 'Hombre', // or 'Mujer'
-  age: '24'
-};
-
-module.exports = { config };
