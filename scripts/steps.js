d897ef475043bfa7d508a254f416b669b59947be
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2025-07-05 13:38:09 +0200

    add random timeouts

--- scripts/steps.js
+++ scripts/steps.js
@@ -2,6 +2,7 @@ const { config } = require('../config/constants');
 const { log } = require('./utils');
 
 async function handleCookies(page) {
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   try {
     const frames = page.frames();
     for (const frame of frames) {
@@ -20,23 +21,38 @@ async function handleCookies(page) {
 
 
 async function selectProfessional(page) {
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=Por Profesional');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.locator("[name='professionalSearch']").fill(config.doctor);
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click(`text=${config.doctor}`);
+  await page.waitForTimeout(1000 + Math.random() * 1000);
 }
 
 async function fillPatientInfo(page) {
   await page.click('text=Siguiente');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=NO, ya estoy en un proceso médico');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=Consulta Sucesiva');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=Telefónica');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=Siguiente');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=Tengo seguro médico');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click(`text=${config.insurance}`);
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click(`text=${config.insurancePlan}`);
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=Continuar');
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click(`text=${config.gender}`);
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.locator("[name='edad']").fill(config.age);
+  await page.waitForTimeout(1000 + Math.random() * 1000);
   await page.click('text=Ver fechas');
 }
 
