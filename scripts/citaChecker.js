96ce1a62a05d79f9376c0075651c2a8a98fb9789
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2025-07-05 18:28:36 +0200

    [outdated] trying useless stuff to bypass bot blocker

--- scripts/citaChecker.js
+++ scripts/citaChecker.js
@@ -4,10 +4,22 @@ const { getLastDate, saveNewDate } = require('../lib/storage');
 const { notify } = require('../lib/notify');
 const { log } = require('./utils');
 
+const userAgentStrings = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.2227.0 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.3497.92 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+];
+
+
 module.exports = async function citaChecker() {
   log('========== Cita Quiron Bot Job Started ==========');
   const browser = await chromium.launch({ headless: false });
-  const page = await browser.newPage();
+  const context = await browser.newContext({
+    userAgent: userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)],
+  });
+  await context.addInitScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+  const page = await context.newPage();
 
   await page.goto('https://www.quironsalud.com/es/cita-medica#r!/n!/1?dir=none');
   await page.waitForLoadState('networkidle');
@@ -36,6 +48,5 @@ module.exports = async function citaChecker() {
       log('This slot is not earlier than the previously saved one:', lastDateStr);
     }
   }
-
   await browser.close();
 };
