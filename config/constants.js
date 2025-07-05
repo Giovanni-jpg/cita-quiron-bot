7d91b272f953bafdcad2b4835b0dad29826c53d9
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2025-07-05 10:17:59 +0200

    ignore future modifications of the config file

--- config/constants.js
+++ config/constants.js
@@ -1,9 +1,9 @@
 const config = {
-  doctor: "malagelada",
-  age: "25",
-  gender: "Mujer",
-  insurance: "ADESLAS",
-  insurancePlan: "POLIZA/COLECTIVOS PRIVADOS"
+  doctor: 'Dr. Name',
+  insurance: 'Your Insurance',
+  insurancePlan: 'Plan Name',
+  gender: 'Hombre', // or 'Mujer'
+  age: '24'
 };
 
 module.exports = { config };
