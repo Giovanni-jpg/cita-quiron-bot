faebf153849bed48b13b78b40279b425ee5efb17
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2025-07-05 18:29:30 +0200

    adding a date formatter for the notification

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,4 +3,18 @@ function log(...args) {
   console.log(`[${logDateTime.toDateString()} - ${logDateTime.toLocaleTimeString()}]`, ...args);
 }
 
-module.exports = { log };
+
+function monthName(monthNumber) {
+  return [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ][monthNumber - 1];
+}
+
+function formatDate(dateTimeStr) {
+  const [dateStr, timeStr] = dateTimeStr.split('_');
+  const [day, month, year] = dateStr.split('/').map(Number);
+  return `${day} ${monthName(month)} ${year} at ${timeStr}`;
+}
+
+module.exports = { log, formatDate };
