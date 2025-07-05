f73aa12666b7e2da9bf3e53510095a25a96d39fb
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2025-07-05 18:27:19 +0200

    new logic to fetch data

---
+++ scripts/fetch-appointments.js
@@ -0,0 +1,97 @@
+const axios = require('axios');
+const qs = require('qs');
+require('dotenv').config();
+const { config } = require('../config/constants');
+const { getLastDate, saveNewDate } = require('../lib/storage');
+const { notify } = require('../lib/notify');
+const { log, formatDate } = require('./utils');
+
+async function fetchAppt() {
+  try {
+    const headers = {
+      accept: '*/*',
+      'accept-language': 'fr-FR,fr;q=0.6',
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      origin: 'https://www.quironsalud.com',
+      referer: 'https://www.quironsalud.com/es/cita-medica',
+      'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+      'x-proxia-csrf': process.env.QUIRON_CSRF,
+      'x-dtpc': '5$309541963_646h47vPQSWQWSOMKAPKHMGPRNQUIOBUUCUUBBD-0e0',
+      'x-dtreferer': 'https://www.quironsalud.com/es/cita-medica',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Brave";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'sec-gpc': '1',
+      cookie: process.env.QUIRON_COOKIES, // full cookie string from cURL (in network tab right click the request and copy as cURL)
+    };
+
+    const payload = qs.stringify({
+      codCitacion: 1,
+      idCentro: config.idCentro,
+      codCentroAsociado: config.codCentroAsociado,
+      idPrestacion: config.idPrestacion,
+      idGarante: 5,
+      idEspecialidad: 31,
+      idGestion: config.idGestion,
+      financialType: 1,
+      garanteHIS: false,
+      espPresHIS: false,
+      fechaInicio: '05/07/2025',
+      idProvincia: 8,
+      idProfesionalPdP: 2186,
+      edad: 25,
+      isProtocolosChatBot: false,
+      isLast: false,
+      tipoBusqueda: 'SIMPLE',
+      horaInicio: 144043,
+    });
+
+    const response = await axios.post(
+      'https://www.quironsalud.com/idcsalud-client/cm/quironsalud/pdp-api/v1/citas/huecos',
+      payload,
+      { headers }
+    );
+    const appointments = response.data;
+
+    if (appointments.length > 0) {
+      const first = appointments[0];
+      const dateTimeStr = `${first.fechaCitaStr}_${first.horaCitaStr}`;
+      log('First slot found', dateTimeStr);
+
+      const lastDateStr = getLastDate();
+      if (!lastDateStr) {
+        log(
+          'No data found in lastAppointment.txt, saving this slot as the earliest one...'
+        );
+        saveNewDate(dateTimeStr);
+      } else {
+        if (dateTimeStr < lastDateStr) {
+          log(
+            'This slot is earlier than the previously saved one! Sending notification...'
+          );
+          const messageDate = formatDate(dateTimeStr);
+          await notify(messageDate);
+          saveNewDate(dateTimeStr);
+        } else {
+          log(
+            'This slot is not earlier than the previously saved one:',
+            lastDateStr
+          );
+        }
+      }
+    } else {
+      console.log('No available appointments found.');
+    }
+  } catch (err) {
+    console.error('Request failed:', err.response?.data || err.message);
+  }
+};
+
+module.exports = { fetchAppt };
+
+
+
