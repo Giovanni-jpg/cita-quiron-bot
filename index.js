5cc45962ee277041f0202174fae03cc7df8a47e1
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2025-07-06 13:13:11 +0200

    add node cron to the index

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const { fetchAppt } = require('./scripts/fetch-appointments');
+const { overrideConsole } = require('./lib/logToFile');
+overrideConsole(); // Capture all console logs/errors
 
-(async () => {
-  await fetchAppt();
-})();
+const { fetchAppt } = require('./scripts/fetch-appointments');
+const cron = require('node-cron');
+
+cron.schedule('*/5 * * * * *', async () => {
+  console.log(`RUNNING CRON JOB`);
+  try {
+    await fetchAppt();
+    console.log(`CRON JOB COMPLETED`);
+  } catch (error) {
+    console.error(`Task failed:`, error);
+  }
+});
